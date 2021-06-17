671c55e2c802c0ac5c4ebca554db24c35c2a0dbb
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-17 21:19:28 +0530

    Create shivansh.py

---
+++ shivansh.py
@@ -0,0 +1,6 @@
+import os 
+#taken from Lion X ub 
+#For Lucifer userbot
+system("https://github.com/kaal0408/LUCIFER-USERBOT.git && cd LUCIFER-USERBOT && python3 ")
+############
+print("lol u got error please inform it in userbot support group")
