3da8c7d595ca1259cf55323946c987407e262502
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-18 19:02:32 +0530

    Delete shivansh.py

--- shivansh.py
+++
@@ -1,6 +0,0 @@
-from os import system
-#taken from Lion X ub 
-#For Lucifer userbot
-system("git clone https://github.com/kaal0408/LUCIFER-USERBOT.git && cd LUCIFER-USERBOT && python3 -m userbot")
-############
-print("lol u got error please inform it in userbot support group")
