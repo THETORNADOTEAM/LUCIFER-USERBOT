a5555b15b70a6985958f3df88e5f1fd4fad06141
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-18 19:18:53 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,17 @@
+import os 
+import subprocess
+from logging import DEBUG, INFO, basicConfig, getLogger, warning
+basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
+LOGS = getLogger("Helper")
+os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA darkcobra")
+os.chdir("darkcobra")
+process = subprocess.Popen(
+        ["python3", "-m", "userbot"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,)
+out, er = process.communicate()
+if er:
+    LOGS.warning(er.decode())
+print("::::::::::::::")
+if out:
+    LOGS.info(out.decode())
