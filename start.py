2ca3b84dde22e44b9e9b37033943f06275ac0d3b
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-18 23:27:18 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ import subprocess
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA darkcobra")
+os.system("git clone https://github.com/kaal0408/LUCIFER-USERBOT")
 os.chdir("darkcobra")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
