840ef0200b2c1b91ef27a1ec6150dcf4dfc2f4c7
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-24 09:31:20 +0530

    Add files via upload

---
+++ userbot/plugins/history.py
@@ -0,0 +1,77 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from LUCIFERbot import bot, CmdHelp
+from LUCIFERbot.utils import admin_cmd, edit_or_reply as eor, sudo_cmd
+
+@mafiabot.on(admin_cmd(pattern="history ?(.*)"))
+@mafiabot.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
+async def _(LUCIFERevent):
+    if LUCIFERevent.fwd_from:
+        return 
+    if not LUCIFERevent.reply_to_msg_id:
+       await eor(LUCIFERevent, "`Please Reply To A User To Get This Module Work`")
+       return
+    reply_message = await LUCIFERevent.get_reply_message() 
+    chat = "Sangmatainfo_bot"
+    victim = reply_message.sender.id
+    if reply_message.sender.bot:
+       await eor(LUCIFERevent, "Need actual users. Not Bots")
+       return
+    await eor(LUCIFERevent, "Checking...")
+    async with LUCIFERevent.client.conversation(chat) as conv:
+          try:     
+              response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response3 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await conv.send_message("/search_id {}".format(victim))
+              response1 = await response1 
+              response2 = await response2 
+              response3 = await response3 
+          except YouBlockedUserError: 
+              await LUCIFERevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
+              return
+          if response1.text.startswith("No records found"):
+             await eor(LUCIFERevent, "User never changed his Username...")
+          else: 
+             await LUCIFERevent.delete()
+             await LUCIFERevent.client.send_message(LUCIFERevent.chat_id, response2.message)
+
+@mafiabot.on(admin_cmd(pattern="unh ?(.*)"))
+@mafiabot.on(sudo_cmd(pattern="unh ?(.*)", allow_sudo=True))
+async def _(LUCIFERevent):
+    if LUCIFERevent.fwd_from:
+        return 
+    if not LUCIFERevent.reply_to_msg_id:
+       await eor(LUCIFERevent, "`Please Reply To A User To Get This Module Work`")
+       return
+    reply_message = await LUCIFERevent.get_reply_message() 
+    chat = "Sangmatainfo_bot"
+    victim = reply_message.sender.id
+    if reply_message.sender.bot:
+       await eor(LUCIFERevent, "Need actual users. Not Bots")
+       return
+    await eor(LUCIFERevent, "Checking...")
+    async with LUCIFERevent.client.conversation(chat) as conv:
+          try:     
+              response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response3 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await conv.send_message("/search_id {}".format(victim))
+              response1 = await response1 
+              response2 = await response2 
+              response3 = await response3 
+          except YouBlockedUserError: 
+              await LUCIFERevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
+              return
+          if response1.text.startswith("No records found"):
+             await eor(LUCIFERevent, "User never changed his Username...")
+          else: 
+             await LUCIFERevent.delete()
+             await LUCIFERevent.client.send_message(LUCIFERevent.chat_id, response3.message)
+
+CmdHelp("history").add_command(
+  "history", "<reply to a user>", "Fetches the name history of replied user."
+).add_command(
+  "unh", "<reply to user>", "Fetches the Username History of replied users."
+).add()
